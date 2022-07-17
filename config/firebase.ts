0aac74a35aa422806ab92160dfcfbafa4678f89b
guymeiri <guymeiri@microsoft.com>
2022-07-17 23:50:47 +0300

    first commit

---
+++ config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB2HzEVS_nlU4hUXWYZ5g5jScbDChPrmC4",
+  authDomain: "nextjs-firebase-auth-5d94b.firebaseapp.com",
+  projectId: "nextjs-firebase-auth-5d94b",
+  storageBucket: "nextjs-firebase-auth-5d94b.appspot.com",
+  messagingSenderId: "609947517157",
+  appId: "1:609947517157:web:637324eacf8258ac8e540c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
